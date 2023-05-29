6d60012fbe8b04efe574a6433f013bef8b910a15
root <root@DESKTOP-70H7GC1>
2023-05-29 10:44:17 +0800

    fix: html dir

--- build/build.sh
+++ build/build.sh
@@ -5,6 +5,7 @@ mkdir $BASEPATH/target
 cd $BASEPATH
 go build -o appexe
 cp -rf $BASEPATH/bin $BASEPATH/target/bin
+cp -rf $BASEPATH/static $BASEPATH/target/static
 mv $BASEPATH/appexe $BASEPATH/target/bin/appexe
 cp -rf $BASEPATH/target $BASEPATH/build/$APPDIR
 ls $BASEPATH/target
